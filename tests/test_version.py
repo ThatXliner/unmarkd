f632b864fc147135a04625a2fd457c9f39ecaa1f
Bryan Hu <bryan.hu.2020@gmail.com>
2021-02-21 15:43:54 -0800

    ✅ Updated tests
    +Added tests for nested tags
    +Added tests for single tags
    Fixed versioning test (also renamed it to test_version)
    !TODO: Full codecoverage and use Hypothesis

--- tests/test_version.py
+++ tests/test_version.py
@@ -9,7 +9,6 @@ project_dir = Path(__file__).parent.parent
 def test_version():
     assert (
         __version__
-        == "0.1.0"
         == toml.loads(project_dir.joinpath("pyproject.toml").read_text())["tool"][
             "poetry"
         ]["version"]
