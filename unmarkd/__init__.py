94fe9733677a8ad262f5a48affbc29a8616a2baf
Bryan Hu <bryan.hu.2020@gmail.com>
2021-02-21 14:12:29 -0800

    :rotating_light: Updated types for unmarkd.unmark
    From just
    (str) -> str
    to
    (Union[str, bs4.NavigableString, bs4.BeautifulSoup]) -> str

--- unmarkd/__init__.py
+++ unmarkd/__init__.py
@@ -1,10 +1,14 @@
 """A markdown reverser."""
+from typing import Union
+
+import bs4  # type: ignore
+
 from . import unmarkers
 
 
-def unmark(html: str) -> str:
+def unmark(html: Union[str, bs4.NavigableString, bs4.BeautifulSoup]) -> str:  # type: ignore
     """Convert HTML to markdown"""
-    return unmarkers.BasicUnmarker().unmark(html)
+    return unmarkers.BasicUnmarker().unmark(html)  # type: ignore
 
 
 __version__ = "0.1.0"
