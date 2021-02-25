cd0250445fd5ad8993cbba5eb614540418dfe5d5
ThatXliner <bryan.hu.2020@gmail.com>
2021-02-25 13:22:39 -0800

    ✅ Added line for better test output

--- tests/test_roundtrip.py
+++ tests/test_roundtrip.py
@@ -1,6 +1,3 @@
-# This test code was written by the `hypothesis.extra.ghostwriter` module
-# and is provided under the Creative Commons Zero public domain dedication.
-
 import commonmark.main
 from hypothesis import assume, given, example
 from hypothesis import strategies as st
@@ -15,5 +12,6 @@ import unmarkd
 def test_roundtrip_commonmark_unmark(text):
     assume(text.strip() == text)
     value0 = commonmark.main.commonmark(text=text)
-    value1 = commonmark.main.commonmark(unmarkd.unmark(html=value0))
-    assert value0 == value1, (value0, value1)
+    reversed = unmarkd.unmark(html=value0)
+    value1 = commonmark.main.commonmark(reversed)
+    assert value0 == value1, (value0, value1, reversed)
