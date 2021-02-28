7bf53f0ec9b73ba9bb0a4b1971dc4b45024f45ec
Bryan Hu <bryan.hu.2020@gmail.com>
2021-02-27 16:44:32 -0800

    :white_check_mark: :art: Restructured tests

--- tests/test_roundtrip.py
+++ tests/test_roundtrip.py
@@ -3,23 +3,39 @@ import unmarkd
 from hypothesis import assume, example, given
 from hypothesis import strategies as st
 
+def helper(text: str) -> None:
+    value0        = commonmark.main.commonmark(text)
+    unmarked      = unmarkd.unmark(html=value0)
+    value1        = commonmark.main.commonmark(unmarked)
+    assert value0 == value1, (value0, value1, unmarked)
 
 @given(text=st.text())
-@example("")
-@example("` `")
-@example("0\n\n0")
-@example("```\n```")
-@example("0.")
-@example("```")
-@example(R"*\**")
-@example(R"**\***")
-@example(R"`\``")
-@example("-")
-@example(">")
-@example("<")
+@example(
+"""<ul>
+<li>tb
+<ol>
+<li>i1</li>
+<li>i2</li>
+<li>i3</li>
+</ol>
+</li>
+<li>bb</li>
+</ul>"""
+)
 def test_roundtrip_commonmark_unmark(text):
     assume(text.strip() == text)
-    value0 = commonmark.main.commonmark(text=text)
-    unmarked = unmarkd.unmark(html=value0)
-    value1 = commonmark.main.commonmark(unmarked)
-    assert value0 == value1, (value0, value1, unmarked)
+    helper(text)
+
+class TestExampleCases:
+    def test_example_1(self):  helper('')
+    def test_example_2(self):  helper('` `')
+    def test_example_3(self):  helper(text="0\n\n0")
+    def test_example_4(self):  helper(text="```\n```")
+    def test_example_5(self):  helper(text="0.")
+    def test_example_6(self):  helper(text="```")
+    def test_example_7(self):  helper(text=R"*\**")
+    def test_example_8(self):  helper(text=R"**\***")
+    def test_example_9(self):  helper(text=R"`\``")
+    def test_example_10(self): helper(text=R"-")
+    def test_example_11(self): helper(text=R">")
+    def test_example_12(self): helper(text=R"<")
